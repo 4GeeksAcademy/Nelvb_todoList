6e79b5f6cacbfe1910c1bfc3b8b585e55910672a
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-08-09 11:51:35 +0000

    cambios css y contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,11 +3,13 @@ import Tareas from "./tareas"; // Importa el componente Tareas desde el archivo 
 
 const Home = () => { // Define el componente Home como una función de flecha.
     const [toDoTasks, setToDoTasks] = useState([]);
+      // Paso 1: Definir la lógica condicional para el plural
+      const taskText = toDoTasks.length === 1 ? 'tarea' : 'tareas';
     return ( // Lo que esta función retorna es lo que se va a renderizar en la pantalla.
         <div className="container"> {/* Crea un div con la clase "container". Esto ayuda a estilizar el componente. */}
             <h1>TO DO LIST</h1> {/* Añade un encabezado h1 con el texto "TO DO LIST". */}
             <Tareas toDoTasks = {toDoTasks} setToDoTasks = {setToDoTasks}/> {/* Incluye el componente Tareas. Esto es como decirle a React "inserta el componente Tareas aquí". */}
-            <p className = 'contador'>tengo {toDoTasks.length} tareas.</p>
+            <p className = 'contador'>Tengo {toDoTasks.length} {taskText} por realizar.</p>
         </div>
     );
 };
